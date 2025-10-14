16b17203a16f015694b38ec460f872f684fcdc95
bladestery <benbeyblade@hotmail.com>
2025-10-15 01:39:05 +0700

    Revert changes

--- app/workflows/graphs/rag/components/question_rewriter.py
+++ app/workflows/graphs/rag/components/question_rewriter.py
@@ -49,31 +49,31 @@ class QuestionRewriter:
             #    strict=True,
             #)
 
-            # from langchain_google_genai import ChatGoogleGenerativeAI
+            from langchain_google_genai import ChatGoogleGenerativeAI
 
-            # self.llm = ChatGoogleGenerativeAI(
-            #    model=LLMModelMap.QUESTION_REWRITER,
-            #    temperature=0,
-            #    max_tokens=None,
-            #    timeout=None,
-            #    max_retries=2,
-            # ).with_structured_output(
-            #    schema=RefinedQueryResult,
-            #    strict=True,
-            # )
-            from langchain_google_vertexai import ChatVertexAI
-
-            self.llm = ChatVertexAI(
-                # pick a Vertex Gemini model available in your region, e.g.:
-                model=LLMModelMap.QUESTION_REWRITER,   # or "gemini-2.5-pro"
-                temperature=0,
-                max_tokens=None,
-                max_retries=2,
+            self.llm = ChatGoogleGenerativeAI(
+               model=LLMModelMap.QUESTION_REWRITER,
+               temperature=0,
+               max_tokens=None,
+               timeout=None,
+               max_retries=2,
             ).with_structured_output(
-                schema=RefinedQueryResult,
-                strict=True
-                # optional: include_raw=True, method="json_mode"
+               schema=RefinedQueryResult,
+               strict=True,
             )
+            # from langchain_google_vertexai import ChatVertexAI
+
+            # self.llm = ChatVertexAI(
+            #     # pick a Vertex Gemini model available in your region, e.g.:
+            #     model=LLMModelMap.QUESTION_REWRITER,   # or "gemini-2.5-pro"
+            #     temperature=0,
+            #     max_tokens=None,
+            #     max_retries=2,
+            # ).with_structured_output(
+            #     schema=RefinedQueryResult,
+            #     strict=True
+            #     # optional: include_raw=True, method="json_mode"
+            # )
 
     @staticmethod
     def delete_messages(state: AgentState) -> dict[str, list]:
